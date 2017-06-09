8e08dacfccb58b4e584ea32e1f09c122cf89fd67
Fabian Frei <fabian.frei@supsi.ch>
2017-06-09 14:45:42 +0200

    Enabled non working config

--- config/config.exs
+++ config/config.exs
@@ -34,7 +34,7 @@ use Mix.Config
 config :extwitter, :oauth, [
  	consumer_key: "6dB5IVeBSvTZx3SC8xTcHoDbO",
   	consumer_secret: "dnosZkvsW8oei56UyCl1uxMqVrR8b3bp5vJdGZRAaA3ga40O0N",
-   	access_token: "166818158-x5QXTEunwhemUa1k8hcFTDrdaCWL00sQNxkMzglt",
+    access_token: "166818158-x5QXTEunwhemUa1k8hcFTDrdaCWL00sQNxkMzglt",
   	access_token_secrect: "ODJDmlQWvmlGpmaxY22wGBxBiikWzNh0NZqBdMFs39TQb"
 ]
 
